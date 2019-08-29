1e66821fd1bfb35d6e4907ae60e04ed83d6e52bd
Joel Wasserman <joel.wasserman8@gmail.com>
2019-08-28 20:19:50 -0700

    back to the b4s1cs

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const { spawn } = require('child_process')
 const { Box, Text, Color } = require('ink')
 const { default: Spinner } = require('ink-spinner')
 const fetch = require('node-fetch')
+const termSize = require('term-size')
 
 class App extends React.Component {
 	constructor (props) {
@@ -24,6 +25,7 @@ class App extends React.Component {
 	componentDidMount () {
 		// kick off ad fetching
 		this.getAd()
+		setInterval(() => this.getAd(), 2000)
 
 		// kick off child command
 		const child = spawn(this.state.command, this.state.args)
@@ -41,15 +43,62 @@ class App extends React.Component {
 
 	async getAd () {
 		// fetch ad and format properly
-		const res = await fetch('https://npmc-api-test.joelwasserman.now.sh/api/ad')
-		const json = await res.json()
-		const { url, title, body } = json
+		// const res = await fetch('https://npmc-api-test.joelwasserman.now.sh/api/ad')
+		// const json = await res.json()
+		// const { url, title, body } = json
+		const messages = [
+			{ // for testing
+				"title": "LogRocket",
+				"body": "Stop guessing why bugs happen. LogRocket lets you replay what users do on your web app or website, helping you reproduce bugs and fix issues faster.",
+				"url": "https://logrocket.com/term"
+			},
+			{ // for testing
+				"title": "Hello Peter",
+				"body": "Donate to teacherfund and get a free t shirt today!",
+				"url": "https://theteacherfund.com/term"
+			},
+			{ // for testing
+				"title": "Trump sucks",
+				"body": "blahhhhhh!",
+				"url": "https://theteacherfund.com/term"
+			}
+		]
+		let { url, title, body } = messages[Math.floor(Math.random() * 2)]
+
+		const adWidth = termSize().columns / 2
+		const topBorder = `┌${'─'.repeat(adWidth)}┐`
+		const bottomBorder = `└${'─'.repeat(adWidth)}┘`
+
+		const colors = [
+			'#FF5733', // redish
+			'#D4AC0D', // yellowish
+			'#633974', // dark purple
+			'#3498DB', // blue
+			'#FBFCFC', // off white
+		]
+
+		const topBorderColor = colors[Math.floor(Math.random() * (colors.length - 1))]
+		const bottomBorderColor = colors[Math.floor(Math.random() * (colors.length - 1))]
+
+		const maxBodyWidth = adWidth - 10
+		const bodyLineCount = body.length / maxBodyWidth
+		const bodyLines = []
+		for (let i = 0; i < bodyLineCount; i++) {
+			const lineText = body.slice(i * maxBodyWidth, (i+1) * maxBodyWidth)
+			bodyLines.push(<Text>{lineText}</Text>)
+		}
 
 		this.setState({ ad: (
-				<Box flexDirection="column" padding={1}>
+				<Box flexDirection="column" 
+					width={adWidth} 
+					alignItems="center"
+					padding={1}>
+					<Color hex={topBorderColor}>{topBorder}</Color>
 					<Text bold>{title}</Text>
-					<Text>{body}</Text>
-					<Text blue>{url}</Text>
+					{bodyLines}
+					<Text> </Text>
+					<Text>{url}</Text>
+					<Color hex={bottomBorderColor}>{bottomBorder}</Color>
 				</Box>
 			)
 		})
@@ -67,20 +116,20 @@ class App extends React.Component {
 	render () {
 		return (
 			<Box paddingY={2} flexDirection="column">
-        <Box paddingY={1} width="100%" flexDirection="column">
-					<Text>
-						{this.state.output.length
-							? this.state.output.join('')
-							: <Spinner type="dots" />
-						}
-					</Text>
+				<Box paddingY={1} width="100%" flexDirection="column">
+							<Text>
+								{this.state.output.length
+									? this.state.output.join('')
+									: <Spinner type="dots" />
+								}
+							</Text>
+						</Box>
+						<Box width="100%" justifyContent="center">
+					<Box>
+						{this.state.ad}
+					</Box>
 				</Box>
-				<Box width="100%" justifyContent="center">
-            <Box>
-                {this.state.ad}
-            </Box>
-        </Box>
-    	</Box>
+			</Box>
 		)
 	}
 }
