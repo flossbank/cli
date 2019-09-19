f42287472d94dd26d9a4542b4510a189b6add8ef
Peter Squicciarini <stripedpajamas273@gmail.com>
2019-09-18 17:04:23 -0700

    Basic yarn support

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-require('./npm')()
-require('./showAd')()
+var ver = process.versions.node;
+var majorVer = parseInt(ver.split('.')[0], 10);
+
+if (majorVer < 4) {
+  console.error('Node version ' + ver + ' is not supported, please use Node.js 4.0 or higher.');
+  process.exit(1); // eslint-disable-line no-process-exit
+}
+
+function main () {
+  var supported = { 'yarn': true, 'npm': true }
+  var pm = process.argv.splice(2, 1)
+  
+  if (!supported[pm]) {
+    console.error('Unsupported Package Manager. NPM and Yarn are currently supported.')
+    process.exit(1)
+  }
+  
+  require(`./${pm}`)()
+  require('./showAd')()
+}
+
+main()
---
+++ yarn.js
@@ -0,0 +1,15 @@
+module.exports = () => {
+  try {
+    require('yarn/lib/v8-compile-cache')
+  } catch (e) {}
+  
+  var cli = require('yarn/lib/cli')
+  cli.default()
+    .then(() => {
+      process.exit(0)
+    })
+    .catch((error) => {
+      console.error(error.stack || error.message || error)
+      process.exitCode = 1
+    })
+}
