cb4a6f5d378f8b5f533ba2cac72df9d95abeaece
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-01-27 12:06:55 -0800

    Configure webpack so that it doesn't get in the way

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
   mode: 'production',
@@ -7,6 +8,18 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'flossbank.bundle.js',
     libraryTarget: 'commonjs2'
+  },
+  optimization: {
+    // concatenateModules: true breaks AbortSignal (node-fetch)
+    concatenateModules: false,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          // keep_classnames: false breaks AbortSignal (node-fetch)
+          keep_classnames: true
+        }
+      })
+    ]
   },
   target: 'node'
 }
