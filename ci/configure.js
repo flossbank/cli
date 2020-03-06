167af4002f0d0bf10ca7339d0f320c2fda4def89
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-03-05 20:39:40 -0800

    Bugfix CI configure

--- ci/configure.js
+++ ci/configure.js
@@ -6,14 +6,14 @@ const INTEG_TEST_KEY = 'cf667c9381f7792bfa772025ff8ee93b89d9a757e6732e87611a0c34
 
 function writeApiKey () {
   return new Promise((resolve, reject) => {
-    execFile('node bin.js source', 'utf8', (err, stdout, stderr) => {
+    execFile('node', ['bin.js', 'source'], (err, stdout) => {
       if (err) reject(err)
       if (!stdout) reject(Error('no source found'))
       const configFile = path.resolve(stdout.trim(), '../config.json')
       const config = require(configFile)
       writeFileSync(configFile, JSON.stringify(Object.assign({}, config, {
         apiKey: INTEG_TEST_KEY
-      })))
+      }), null, 4))
       resolve()
     })
   })
