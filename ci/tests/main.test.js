5b0791f845cb23dfe831220fc322328c953e81f5
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-03-17 22:06:19 -0700

    Clean up integ tests

--- ci/tests/main.test.js
+++ ci/tests/main.test.js
@@ -1,115 +1,36 @@
 const test = require('ava')
 const path = require('path')
-const { promisify } = require('util')
-const { execFile } = require('child_process')
-const { readFile } = require('fs')
-const rimraf = require('rimraf')
-const ls = require('ls')
-const Config = require('../../src/config')
+const util = require('./_util')
+
 const testPkgJson = require('./package.json')
 
 const testNodeDeps = Object.keys(testPkgJson.dependencies)
 
-const rm = promisify(rimraf)
-
-const INTEG_TEST_KEY = 'cf667c9381f7792bfa772025ff8ee93b89d9a757e6732e87611a0c34b48357d1'
-const config = new Config()
-
-let pythonDepDir
-
-function setIntegApiKey () {
-  return config.setApiKey(INTEG_TEST_KEY)
-}
-
-function setInvalidApiKey () {
-  return config.setApiKey('very-invalid-api-key')
-}
-
-function getBinPath () {
-  return path.resolve(process.cwd(), '../../', 'bin.js')
-}
-
-async function deleteInstalledDeps () {
-  if (!pythonDepDir) {
-    pythonDepDir = await resolvePythonDepDir()
-  }
-  return Promise.all([
-    rm('node_modules'),
-    rm(`${pythonDepDir}/*`)
-  ])
-}
-
-async function getNodeModules () {
-  return ls('./node_modules/*').map(dir => dir.name)
-}
-
-function resolvePythonDepDir () {
-  return new Promise((resolve, reject) => {
-    execFile('python', ['-m', 'site', '--user-site'], (err, stdout) => {
-      if (err) return reject(err)
-      resolve(stdout.trim())
-    })
-  })
-}
-
-async function getPythonPackages () {
-  if (!pythonDepDir) {
-    pythonDepDir = await resolvePythonDepDir()
-  }
-  return ls(`${pythonDepDir}/*`).map(dir => dir.name)
-}
-
-function runFlossbank (args) {
-  return new Promise((resolve, reject) => {
-    execFile('node', [getBinPath()].concat(args), (err, stdout) => {
-      if (err) return reject(err)
-      resolve(stdout.trim())
-    })
-  })
-}
-
-function getLastRunlog () {
-  return new Promise((resolve, reject) => {
-    execFile('node', [getBinPath(), 'runlog'], (err, stdout) => {
-      if (err) return reject(err)
-      if (!stdout) return reject(Error('no runlog found'))
-      readFileAsync(stdout.trim()).then((data) => resolve(JSON.parse(data)))
-    })
-  })
-}
-
-function readFileAsync (...args) {
-  return new Promise((resolve, reject) => {
-    readFile(...args, (err, data) => {
-      if (err) return reject(err)
-      resolve(data)
-    })
-  })
-}
-
-test.before(() => {
+test.before(async (t) => {
   process.chdir(__dirname)
+
+  t.context.pythonDepDir = await util.resolvePythonDepDir()
 })
 
 test.afterEach(async (t) => {
   // delete node modules and other deps from the test dir so that we can assert their presence during tests
-  await deleteInstalledDeps()
-  t.log('node modules empty:', await getNodeModules())
-  t.log('python modules empty:', await getPythonPackages())
+  await util.deleteInstalledDeps(t.context.pythonDepDir)
+  t.log('node modules empty:', await util.getNodeModules())
+  t.log('python modules empty:', await util.getPythonPackages(t.context.pythonDepDir))
 })
 
-test.after.always((t) => {
+test.after.always(() => {
   process.chdir(path.resolve(__dirname, '..'))
 })
 
 test.serial('integ: run pm with ads (npm)', async (t) => {
-  await setIntegApiKey()
-  await runFlossbank(['npm', 'install'])
-  const nodeModules = await getNodeModules()
+  await util.setIntegApiKey()
+  await util.runFlossbank(['npm', 'install'])
+  const nodeModules = await util.getNodeModules()
   t.log('installed node modules:', nodeModules)
   t.true(testNodeDeps.every(dep => nodeModules.includes(dep)))
 
-  const runlog = await getLastRunlog()
+  const runlog = await util.getLastRunlog()
   t.true(runlog.supportedPm)
   t.is(runlog.pmCmd, 'npm install')
   t.true(runlog.seenAdIds.length > 0)
@@ -117,13 +38,13 @@ test.serial('integ: run pm with ads (npm)', async (t) => {
 })
 
 test.serial.only('integ: run pm with ads (pip)', async (t) => {
-  await setIntegApiKey()
-  await runFlossbank(['pip', 'install', 'simplejson', '--user'])
-  const pythonPackages = await getPythonPackages()
+  await util.setIntegApiKey()
+  await util.runFlossbank(['pip', 'install', 'simplejson', '--user'])
+  const pythonPackages = await util.getPythonPackages(t.context.pythonDepDir)
   t.log('installed python packages:', pythonPackages)
   t.true(pythonPackages.includes('simplejson'))
 
-  const runlog = await getLastRunlog()
+  const runlog = await util.getLastRunlog()
   t.true(runlog.supportedPm)
   t.is(runlog.pmCmd, 'pip install simplejson --user')
   t.true(runlog.seenAdIds.length > 0)
@@ -131,24 +52,22 @@ test.serial.only('integ: run pm with ads (pip)', async (t) => {
 })
 
 test.serial('integ: run in passthru mode when auth fails', async (t) => {
-  await setInvalidApiKey()
-  await runFlossbank(['npm', 'install'])
-  const nodeModules = await getNodeModules()
+  await util.setInvalidApiKey()
+  await util.runFlossbank(['npm', 'install'])
+  const nodeModules = await util.getNodeModules()
   t.log('installed node modules:', nodeModules)
   t.true(testNodeDeps.every(dep => nodeModules.includes(dep)))
 
-  const runlog = await getLastRunlog()
+  const runlog = await util.getLastRunlog()
   t.true(runlog.supportedPm)
   t.true(runlog.passthrough)
   t.is(runlog.errors.length, 1) // only 1 error (authentication)
-
-  t.context.deletedNodeModules = true
 })
 
 test.serial('integ: install to shell profiles', async (t) => {
-  await runFlossbank(['install'])
+  await util.runFlossbank(['install'])
 
-  const runlog = await getLastRunlog()
+  const runlog = await util.getLastRunlog()
   t.deepEqual(runlog.arguments, { hasArgs: true, install: true })
   const profilePaths = [
     ...runlog.detectedShellFormatProfiles,
@@ -156,15 +75,16 @@ test.serial('integ: install to shell profiles', async (t) => {
   ]
   t.true(profilePaths.length > 0)
   t.log('profile paths:', profilePaths)
-  const profiles = await Promise.all(profilePaths.map(profile => readFileAsync(profile)))
+  const profiles = await Promise.all(profilePaths.map(profile => util.readFileAsync(profile)))
 
   t.true(profiles.every(profile => profile.includes('flossbank_aliases')))
+  t.log('after install all shell profiles were found sourcing flossbank aliases')
 })
 
 test.serial('integ: uninstall from shell profiles', async (t) => {
-  await runFlossbank(['uninstall'])
+  await util.runFlossbank(['uninstall'])
 
-  const runlog = await getLastRunlog()
+  const runlog = await util.getLastRunlog()
   t.deepEqual(runlog.arguments, { hasArgs: true, uninstall: true })
   const profilePaths = [
     ...runlog.detectedShellFormatProfiles,
@@ -172,7 +92,8 @@ test.serial('integ: uninstall from shell profiles', async (t) => {
   ]
   t.true(profilePaths.length > 0)
   t.log('profile paths:', profilePaths)
-  const profiles = await Promise.all(profilePaths.map(profile => readFileAsync(profile)))
+  const profiles = await Promise.all(profilePaths.map(profile => util.readFileAsync(profile)))
 
   t.true(profiles.every(profile => !profile.includes('flossbank_aliases')))
+  t.log('after uninstall no shell profiles were found sourcing flossbank aliases')
 })
