95bed718a26ab18978ab6c5a77e2bb58a50628e8
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-03-17 20:03:27 -0700

    Add integ test for pip install

--- ci/tests/main.test.js
+++ ci/tests/main.test.js
@@ -1,13 +1,22 @@
+/* eslint-disable */
 const test = require('ava')
 const path = require('path')
+const { promisify } = require('util')
 const { execFile } = require('child_process')
 const { readFile } = require('fs')
 const rimraf = require('rimraf')
 const ls = require('ls')
 const Config = require('../../src/config')
+const testPkgJson = require('./package.json')
+
+const testNodeDeps = Object.keys(testPkgJson.dependencies)
+
+const rm = promisify(rimraf)
 
 const INTEG_TEST_KEY = 'cf667c9381f7792bfa772025ff8ee93b89d9a757e6732e87611a0c34b48357d1'
 const config = new Config()
+
+let pythonDepDir
 
 function setIntegApiKey () {
   return config.setApiKey(INTEG_TEST_KEY)
@@ -21,17 +30,34 @@ function getBinPath () {
   return path.resolve(process.cwd(), '../../', 'bin.js')
 }
 
-function deleteNpmArtifacts () {
+async function deleteInstalledDeps () {
+  if (!pythonDepDir) {
+    pythonDepDir = await resolvePythonDepDir()
+  }
+  return Promise.all([
+    rm('node_modules'),
+    rm(`${pythonDepDir}/*`)
+  ])
+}
+
+async function getNodeModules () {
+  return ls('./node_modules/*').map(dir => dir.name)
+}
+
+function resolvePythonDepDir () {
   return new Promise((resolve, reject) => {
-    rimraf('node_modules', (err) => {
+    execFile('python', ['-m', 'site', '--user-site'], (err, stdout) => {
       if (err) return reject(err)
-      return resolve()
+      resolve(stdout.trim())
     })
   })
 }
 
-function getNodeModules () {
-  return ls('./node_modules/*').map(dir => dir.name)
+async function getPythonPackages () {
+  if (!pythonDepDir) {
+    pythonDepDir = await resolvePythonDepDir()
+  }
+  return ls(`${pythonDepDir}/*`).map(dir => dir.name)
 }
 
 function runFlossbank (args) {
@@ -67,39 +93,50 @@ test.before(() => {
 })
 
 test.afterEach(async (t) => {
-  if (t.context.deletedNodeModules) {
-    // delete node modules from the test dir so that we can assert their presence during tests
-    await deleteNpmArtifacts()
-    t.log('node modules empty:', getNodeModules())
-  }
+  // delete node modules and other deps from the test dir so that we can assert their presence during tests
+  await deleteInstalledDeps()
+  t.log('node modules empty:', await getNodeModules())
+  t.log('python modules empty:', await getPythonPackages())
 })
 
 test.after.always((t) => {
   process.chdir(path.resolve(__dirname, '..'))
 })
 
-test.serial('integ: run pm with ads', async (t) => {
+test.serial('integ: run pm with ads (npm)', async (t) => {
   await setIntegApiKey()
   await runFlossbank(['npm', 'install'])
-  const nodeModules = getNodeModules()
+  const nodeModules = await getNodeModules()
   t.log('installed node modules:', nodeModules)
-  t.true(nodeModules.length > 0)
+  t.true(testNodeDeps.every(dep => nodeModules.includes(dep)))
 
   const runlog = await getLastRunlog()
   t.true(runlog.supportedPm)
   t.is(runlog.pmCmd, 'npm install')
   t.true(runlog.seenAdIds.length > 0)
   t.false(runlog.passthrough)
+})
 
-  t.context.deletedNodeModules = true
+test.serial.only('integ: run pm with ads (pip)', async (t) => {
+  await setIntegApiKey()
+  await runFlossbank(['pip', 'install', 'simplejson', '--user'])
+  const pythonPackages = await getPythonPackages()
+  t.log('installed python packages:', pythonPackages)
+  t.true(pythonPackages.includes('simplejson'))
+
+  const runlog = await getLastRunlog()
+  t.true(runlog.supportedPm)
+  t.is(runlog.pmCmd, 'pip install simplejson --user')
+  t.true(runlog.seenAdIds.length > 0)
+  t.false(runlog.passthrough)
 })
 
 test.serial('integ: run in passthru mode when auth fails', async (t) => {
   await setInvalidApiKey()
   await runFlossbank(['npm', 'install'])
-  const nodeModules = getNodeModules()
+  const nodeModules = await getNodeModules()
   t.log('installed node modules:', nodeModules)
-  t.true(nodeModules.length > 0)
+  t.true(testNodeDeps.every(dep => nodeModules.includes(dep)))
 
   const runlog = await getLastRunlog()
   t.true(runlog.supportedPm)
