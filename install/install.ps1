9b9b87a714e09398361e6409bfcbe8ec254e6f54
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-05-17 13:44:30 -0700

    Update install.ps1 license

--- install/install.ps1
+++ install/install.ps1
@@ -1,7 +1,23 @@
 #!/usr/bin/env pwsh
-# Copyright 2020 the Flossbank authors. All rights reserved. MIT license.
 # TODO(everyone): Keep this script simple and easily auditable.
 # Thanks deno.land for inspiration <3
+#
+#    Copyright (C) 2020 Flossbank, Inc.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 $ErrorActionPreference = 'Stop'
 
 $Target = 'win-x86_64'
