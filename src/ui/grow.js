b528fcdda4acd43392c40cde40cdab9351ed996a
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-07-08 10:05:28 -0700

    Add growing ad utility

---
+++ src/ui/grow.js
@@ -0,0 +1,60 @@
+const Ui = require('./index.js')
+
+function* getGrowingAd (attr) {
+	if (!['title', 'body', 'url'].includes(attr)) {
+		throw new Error('invalid attr')
+	}
+	let width = 40
+	while (true) {
+		switch (attr) {
+			case 'title': {
+				yield {
+					title: 'A'.repeat(width),
+					body: `Title Length Is ${width}`,
+					url: 'https://google.com',
+				}
+				break
+			}
+			case 'body': {
+				yield {
+					title: `Body Length Is ${width}`,
+					body: 'A'.repeat(width),
+					url: 'https://google.com',
+				}
+				break
+			}
+			case 'url': {
+				yield {
+					title: `URL Length Is ${width}`,
+					body: 'On the banks of the river Floss',
+					url: 'A'.repeat(width),
+				}
+				break
+			}
+		}
+		width++
+	}
+}
+
+
+async function main () {
+	const attr = process.argv[2]
+	const growingAd = getGrowingAd(attr)
+	const config = {
+		getUpdateAvailable: () => false
+	}
+	const client = {
+		getAd: () => growingAd.next().value,
+		getSeenAds: () => []
+	}
+	const runlog = {
+		debug: () => {}
+	}
+	const stdout = process.stdout
+	
+	const ui = new Ui({ config, client, runlog, stdout })
+	ui.interval = 200
+	ui.startAds({ pmCmd: 'nothing' })
+}
+
+main()
